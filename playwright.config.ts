e66ca67c2c26008f4264887c681959aaa5ad155b
CropWatch <111664979+CropWatchDevelopment@users.noreply.github.com>
2025-06-14 13:36:47 +0900

    vendor modules locally

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	},
+        webServer: {
+                command: 'PUBLIC_RECAPTCHA_SITE_KEY=test PRIVATE_GOOGLE_RECAPTCHA_SECRET_KEY=test PRIVATE_EMAIL_HOST=localhost PRIVATE_EMAIL_PORT=1025 PRIVATE_EMAIL_USERNAME=user PRIVATE_EMAIL_PASSWORD=pass npm run build && npm run preview',
+                port: 4173
+        },
 
-	testDir: 'e2e'
+        testDir: 'e2e'
 });
