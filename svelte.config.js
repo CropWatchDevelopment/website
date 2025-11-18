ae7ddc286c993267fab823272a6b56ac88af4622
Kevin Cantrell <kevin@cropwatch.io>
2025-11-18 14:33:37 +0900

    adding image optimization in images

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,18 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp', 'image/jpeg', 'image/jpg', 'image/png'],
+				minimumCacheTTL: 300,
+				domains: ['cropwatch.io', 'website-iilojvva6-crop-watch-team.vercel.app'],
+			},
+		}),
+
+	},
 	vitePlugin: {
 		inspector: {
 			toggleKeyCombo: 'alt-x',
